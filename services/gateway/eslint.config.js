03fc182b96eb8c3a13ca15bb0fe637af78fc447b
Dmitry Troshkin <186717460+troshkindm@users.noreply.github.com>
2025-11-21 10:22:16 +0300

    refactor: initial structure

---
+++ services/gateway/eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js'
+import prettierConfig from 'eslint-plugin-prettier/recommended'
+import importSort from 'eslint-plugin-simple-import-sort'
+import globals from 'globals'
+import ts from 'typescript-eslint'
+
+export default [
+	js.configs.recommended,
+	...ts.configs.recommended,
+	prettierConfig,
+	{
+		plugins: {
+			'simple-import-sort': importSort,
+		},
+		languageOptions: {
+			globals: {
+				...globals.node,
+			},
+		},
+		rules: {
+			'simple-import-sort/imports': 'error',
+			'simple-import-sort/exports': 'error',
+			'no-console': ['warn', { allow: ['error', 'warn'] }],
+			'prefer-const': 'error',
+			'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+		},
+	},
+]
